4242ee9fa9584f6ae852d1b64e3034bebcfda2c9
auztin410 <auztin410@gmail.com>
2018-07-11 16:01:41 -0400

    Added in rough twitter api functionality.

--- liri.js
+++ liri.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 
 var inquirer = require("inquirer");
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var key = require("./key.js");
 
+console.log(key);
+
+var spotify = new Spotify(key.spotify);
+var client = new Twitter(key.twitter);
 
 
 var promptGo = true;
 
-var askPrompt = function() {
-    if(promptGo === true){
+var askPrompt = function () {
+    if (promptGo === true) {
         inquirer.prompt([
             {
                 type: "list",
@@ -16,49 +23,62 @@ var askPrompt = function() {
                 name: "options"
             }
         ])
-        .then(function(inquirerResponse) {
-            
+            .then(function (inquirerResponse) {
 
-            switch(inquirerResponse.options) {
 
-                case "My Tweets":
-                console.log("I'm twitting my tweeter!");
-                break;
+                switch (inquirerResponse.options) {
 
-                case "Spotify This Song":
-                console.log("Find this song you dumb robot!");
-                break;
+                    case "My Tweets":
+                        console.log("I'm twitting my tweeter!");
+                        var params = { screen_name: 'Antonath' };
+                        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+                            if (!error) {
+                                console.log(tweets);
+                            }
+                        });
+                        break;
 
-                case "Movie This":
-                console.log("I love Movies!");
-                break;
+                    case "Spotify This Song":
+                        console.log("Find this song you dumb robot!");
+                        inquirer.prompt([
+                            {
+                                type: "input",
+                                message: "What song do you want to search for?",
+                                name: "songName"
+                            }
+                        ])
+                        break;
 
-                case "Do What it Says":
-                console.log("I will do what you command!");
-                break;
+                    case "Movie This":
+                        console.log("I love Movies!");
+                        break;
 
-                default: console.log("That is not a valid option.")
-            }
+                    case "Do What it Says":
+                        console.log("I will do what you command!");
+                        break;
 
-            inquirer.prompt([
-                {
-                type: "confirm",
-                message: "Do you want to continue?",
-                name: "continue",
-                default: true
-            }
-            ])
-            .then(function(inquirerResponse) {
-                if(inquirerResponse.continue === false) {
-                    promptGo = false;
-                    console.log("Thanks for using Liri! And have a nice day!")
-                    return;
+                    default: console.log("That is not a valid option.")
                 }
-                else{
-                    askPrompt();
-                }
+
+                inquirer.prompt([
+                    {
+                        type: "confirm",
+                        message: "Do you want to continue?",
+                        name: "continue",
+                        default: true
+                    }
+                ])
+                    .then(function (inquirerResponse) {
+                        if (inquirerResponse.continue === false) {
+                            promptGo = false;
+                            console.log("Thanks for using Liri! And have a nice day!")
+                            return;
+                        }
+                        else {
+                            askPrompt();
+                        }
+                    })
             })
-        })
     }
 }
 
