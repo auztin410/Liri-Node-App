5f2773f0e1e8f679b08636b7831fc46c9a45ef99
auztin410 <auztin410@gmail.com>
2018-07-12 08:04:53 -0400

    Twitter API integration completed!

--- liri.js
+++ liri.js
@@ -29,11 +29,49 @@ var askPrompt = function () {
                 switch (inquirerResponse.options) {
 
                     case "My Tweets":
-                        console.log("I'm twitting my tweeter!");
+                        
                         var params = { screen_name: 'Antonath' };
                         client.get('statuses/user_timeline', params, function (error, tweets, response) {
                             if (!error) {
-                                console.log(tweets);
+                                console.log("Tweet #1: " + tweets[0].text);
+                                console.log("Tweet #2: " + tweets[1].text);
+                                console.log("Tweet #3: " + tweets[2].text);
+                                console.log("Tweet #4: " + tweets[3].text);
+                                console.log("Tweet #5: " + tweets[4].text);
+                                console.log("Tweet #6: " + tweets[5].text);
+                                console.log("Tweet #7: " + tweets[6].text);
+                                console.log("Tweet #8: " + tweets[7].text);
+                                console.log("Tweet #9: " + tweets[8].text);
+                                console.log("Tweet #10: " + tweets[9].text);
+                                console.log("Tweet #11: " + tweets[10].text);
+                                console.log("Tweet #12: " + tweets[11].text);
+                                console.log("Tweet #13: " + tweets[12].text);
+                                console.log("Tweet #14: " + tweets[13].text);
+                                console.log("Tweet #15: " + tweets[14].text);
+                                console.log("Tweet #16: " + tweets[15].text);
+                                console.log("Tweet #17: " + tweets[16].text);
+                                console.log("Tweet #18: " + tweets[17].text);
+                                console.log("Tweet #19: " + tweets[18].text);
+                                console.log("Tweet #20: " + tweets[19].text);
+                                
+                                inquirer.prompt([
+                                    {
+                                        type: "confirm",
+                                        message: "Do you want to continue?",
+                                        name: "continue",
+                                        default: true
+                                    }
+                                ])
+                                    .then(function (inquirerResponse) {
+                                        if (inquirerResponse.continue === false) {
+                                            promptGo = false;
+                                            console.log("Thanks for using Liri! And have a nice day!")
+                                            return;
+                                        }
+                                        else {
+                                            askPrompt();
+                                        }
+                                    })
                             }
                         });
                         break;
@@ -60,24 +98,7 @@ var askPrompt = function () {
                     default: console.log("That is not a valid option.")
                 }
 
-                inquirer.prompt([
-                    {
-                        type: "confirm",
-                        message: "Do you want to continue?",
-                        name: "continue",
-                        default: true
-                    }
-                ])
-                    .then(function (inquirerResponse) {
-                        if (inquirerResponse.continue === false) {
-                            promptGo = false;
-                            console.log("Thanks for using Liri! And have a nice day!")
-                            return;
-                        }
-                        else {
-                            askPrompt();
-                        }
-                    })
+                
             })
     }
 }
